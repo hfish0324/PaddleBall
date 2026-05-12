a1e0a730d5eb039a23ba8231b8fc668fc1883596
Harrison <hsfisher@email.neit.edu>
2026-05-12 18:56:56 -0400

    Code Fixing
    Fixing

--- js/GameObject.js
+++ js/GameObject.js
@@ -1,11 +1,44 @@
-function GameObject(x, y, width, height) {
-    
-    let object = {};
+function GameObject(x, y, width, height, color) {
 
-    object.x = x;
-    object.y = y;
-    object.width = width;
-    object.height = height;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
 
-    return object;
-} 
+    this.vx = 0;
+    this.vy = 0;
+
+    this.ax = 0;
+    this.ay = 0;
+
+    this.color = color;
+}
+
+GameObject.prototype.update = function(deltaTime) {
+
+    // Apply acceleration
+    this.vx = this.vx + this.ax;
+    this.vy = this.vy + this.ay;
+
+    // Update position
+    this.x = this.x + this.vx * deltaTime;
+    this.y = this.y + this.vy * deltaTime;
+};
+
+GameObject.prototype.draw = function(ctx) {
+    ctx.fillStyle = this.color;
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+};
+
+GameObject.prototype.collidesWith = function(other) {
+
+    if (this.x < other.x + other.width &&
+        this.x + this.width > other.x &&
+        this.y < other.y + other.height &&
+        this.y + this.height > other.y) {
+
+        return true;
+    }
+
+    return false;
+};
--- js/main.js
+++ js/main.js
@@ -1,84 +1,154 @@
-var canvas = document.getElementById('gameCanvas');
-var ctx = canvas.getContext('2d');
+var canvas = document.getElementById("gameCanvas");
+var ctx = canvas.getContext("2d");
 
-let paddle = GameObject(350, 550, 100, 20);
-paddle.speed = 0;
+var lastTime = 0;
 
-paddle.acceleration = 0.5;
-paddle.friction = 0.9;
+var gravity = 0.4;
+var friction = 0.99;
+var stringLength = 150;
 
-let ball = GameObject(400, 300, 20, 20);
-ball.speedX = 5;
-ball.speedY = 2;
+/* -------------------------
+   PADDLE
+--------------------------*/
 
-ball.gravity = 0.1;
-
-function drawPaddle() {
-    ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+function Paddle(x, y) {
+    GameObject.call(this, x, y, 120, 20, "cyan");
+    this.speed = 0.6;
 }
 
-function drawBall() {
-    ctx.beginPath();
+Paddle.prototype = Object.create(GameObject.prototype);
 
-    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
-    ctx.fill();
+Paddle.prototype.update = function(deltaTime) {
+
+    this.ax = 0;
+
+    if (keys["ArrowLeft"]) {
+        this.ax = -this.speed;
+    }
+
+    if (keys["ArrowRight"]) {
+        this.ax = this.speed;
+    }
+
+    if (keys["Shift"]) {
+        this.ax = this.ax * 1.8;
+    }
+
+    GameObject.prototype.update.call(this, deltaTime);
+
+    this.vx = this.vx * 0.9;
+
+    if (this.x < 0) this.x = 0;
+    if (this.x + this.width > canvas.width)
+        this.x = canvas.width - this.width;
+};
+
+/* -------------------------
+   BALL
+--------------------------*/
+
+function Ball(x, y) {
+    GameObject.call(this, x, y, 20, 20, "red");
 }
 
-function movePaddle() {
-    if (keys['ArrowLeft']) {
-        paddle.speed -= paddle.acceleration;
-    }
-    if (keys['ArrowRight']) {
-        paddle.speed += paddle.acceleration;
-    }
-    paddle.speed *= paddle.friction;
-    paddle.x += paddle.speed;
+Ball.prototype = Object.create(GameObject.prototype);
 
-    // Prevent paddle from going out of bounds
-    if (paddle.x < 0) {
-        paddle.x = 0;
-        paddle.speed = 0;
+Ball.prototype.update = function(deltaTime) {
+
+    this.ay = gravity;
+
+    GameObject.prototype.update.call(this, deltaTime);
+
+    this.vx = this.vx * friction;
+    this.vy = this.vy * friction;
+
+    // Anchor point (center of paddle top)
+    var anchorX = paddle.x + paddle.width / 2;
+    var anchorY = paddle.y;
+
+    // Ball center
+    var ballCenterX = this.x + this.width / 2;
+    var ballCenterY = this.y + this.height / 2;
+
+    var dx = ballCenterX - anchorX;
+    var dy = ballCenterY - anchorY;
+
+    var distance = Math.sqrt(dx * dx + dy * dy);
+
+    if (distance > stringLength) {
+
+        var angle = Math.atan2(dy, dx);
+
+        ballCenterX = anchorX + Math.cos(angle) * stringLength;
+        ballCenterY = anchorY + Math.sin(angle) * stringLength;
+
+        this.x = ballCenterX - this.width / 2;
+        this.y = ballCenterY - this.height / 2;
+
+        this.vx = this.vx * 0.8;
+        this.vy = this.vy * 0.8;
     }
-    if (paddle.x + paddle.width > canvas.width) {
-        paddle.x = canvas.width - paddle.width;
-        paddle.speed = 0;
-    }
+};
+
+/* -------------------------
+   SETUP
+--------------------------*/
+
+var paddle = new Paddle(canvas.width / 2 - 60, canvas.height - 80);
+var ball = new Ball(
+    canvas.width / 2 - 10,
+    canvas.height - 80 - stringLength
+);
+
+/* -------------------------
+   UPDATE
+--------------------------*/
+
+function update(deltaTime) {
+    paddle.update(deltaTime);
+    ball.update(deltaTime);
 }
 
-function moveBall() {
-    ball.speedY += ball.gravity;
-    ball.x += ball.speedX;
-    ball.y += ball.speedY;
-}
+/* -------------------------
+   DRAW
+--------------------------*/
 
-function wallCollision() {
-    if (ball.x + ball.radius > canvas.width) {
-        ball.speedX *= -1;
+function draw() {
 
-    }
-    if (ball.x - ball.radius < 0) {
-        ball.speedX *= -1;
-    }
-    if (ball.y + ball.radius > canvas.height) {
-        ball.speedY *= -1;
-    }
-}
-
-function paddleCollision() {
-    if (ball.x > paddle.x && ball.x < paddle.x + paddle.width && ball.y + ball.radius > paddle.y) {
-        ball.speedY *= -1;
-    }
-}
-
-function gameLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    movePaddle();
-    moveBall();
-    wallCollision();
-    paddleCollision();
-    drawPaddle();
-    drawBall();
+
+    // Draw string
+    ctx.beginPath();
+    ctx.moveTo(
+        paddle.x + paddle.width / 2,
+        paddle.y
+    );
+
+    ctx.lineTo(
+        ball.x + ball.width / 2,
+        ball.y + ball.height / 2
+    );
+
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+
+    paddle.draw(ctx);
+    ball.draw(ctx);
+}
+
+/* -------------------------
+   LOOP
+--------------------------*/
+
+function gameLoop(timestamp) {
+
+    var deltaTime = (timestamp - lastTime) / 16;
+    lastTime = timestamp;
+
+    update(deltaTime);
+    draw();
+
     requestAnimationFrame(gameLoop);
 }
 
-gameLoop();
+requestAnimationFrame(gameLoop);
