9781492dd075f5178cac3bf5a21370bf426af872
Harrison <hsfisher@email.neit.edu>
2026-05-14 11:18:43 -0400

    Update GameObject.js

--- GameObject.js
+++ GameObject.js
@@ -1,17 +1,82 @@
-function GameObject(x, y, width, height, color)
+function GameObject(x, y, w, h, color)
 {
-    this.x = x;
-    this.y = y;
-    this.width = width;
-    this.height = height;
-
     this.vx = 0;
     this.vy = 0;
 
-    this.ax = 0.5;
-    this.force = 1;
+    if (x == undefined)
+    {
+        this.x = canvas.width / 2;
+    }
+    else
+    {
+        this.x = x;
+    }
 
-    this.color = color;
+    if (y == undefined)
+    {
+        this.y = canvas.height / 2;
+    }
+    else
+    {
+        this.y = y;
+    }
+
+    if (w == undefined)
+    {
+        this.width = 100;
+    }
+    else
+    {
+        this.width = w;
+    }
+
+    if (h == undefined)
+    {
+        this.height = 100;
+    }
+    else
+    {
+        this.height = h;
+    }
+
+    if (color == undefined)
+    {
+        this.color = "#ff0000";
+    }
+    else
+    {
+        this.color = color;
+    }
+
+    this.left = function() { return this.x - this.width / 2; };
+    this.right = function() { return this.x + this.width / 2; };
+    this.top = function() { return this.y - this.height / 2; };
+    this.bottom = function() { return this.y + this.height / 2; };
+
+    this.force = 1;
+    this.ax = 1;
+    this.ay = 1;
+
+    this.drawCircle = function()
+    {
+        context.save();
+        context.fillStyle = this.color;
+        context.beginPath();
+        context.translate(this.x, this.y);
+        context.arc(0, 0, this.width / 2, 0, Math.PI * 2);
+        context.closePath();
+        context.fill();
+        context.restore();
+    };
+
+    this.drawRect = function()
+    {
+        context.save();
+        context.fillStyle = this.color;
+        context.translate(this.x, this.y);
+        context.fillRect(-this.width / 2, -this.height / 2, this.width, this.height);
+        context.restore();
+    };
 
     this.move = function()
     {
@@ -19,32 +84,13 @@ function GameObject(x, y, width, height, color)
         this.y += this.vy;
     };
 
-    this.drawRect = function()
-    {
-        context.fillStyle = this.color;
-        context.fillRect(
-            this.x - this.width / 2,
-            this.y - this.height / 2,
-            this.width,
-            this.height
-        );
-    };
-
-    this.drawCircle = function()
-    {
-        context.fillStyle = this.color;
-        context.beginPath();
-        context.arc(this.x, this.y, this.width / 2, 0, Math.PI * 2);
-        context.fill();
-    };
-
     this.collisionCheck = function(obj)
     {
         return (
-            this.x < obj.x + obj.width / 2 &&
-            this.x > obj.x - obj.width / 2 &&
-            this.y < obj.y + obj.height / 2 &&
-            this.y > obj.y - obj.height / 2
+            this.left() < obj.right() &&
+            this.right() > obj.left() &&
+            this.top() < obj.bottom() &&
+            this.bottom() > obj.top()
         );
     };
 }
